f868248aa620c7c52c896f23163ac612ea075ea7
Adom Hartell <adomhartell@gmail.com>
2017-03-13 21:20:31 -0700

    add api util for friendships

--- frontend/util/friendship_api_util.js
+++ frontend/util/friendship_api_util.js
@@ -1,34 +1,34 @@
-export const findFriend = (friendUserId) => {
-  return $.ajax({
+export const findFriend = (friendUserId) => (
+  $.ajax({
     method: 'GET',
     url: `/api/users/${friendUserId}/friendships`
-  });
-};
+  })
+);
 
-export const createFriend = (friendUserId) => {
-  return $.ajax({
+export const createFriend = (friendUserId) => (
+  $.ajax({
     method: 'POST',
     url: `/api/users/${friendUserId}/friendships`
-  });
-};
+  })
+);
 
-export const removeFriend = (friendshipId) => {
-  return $.ajax({
+export const removeFriend = (friendshipId) => (
+  $.ajax({
     method: 'DELETE',
     url: `/api/friendships/${friendshipId}`
-  });
-};
+  })
+);
 
-export const acceptFriend = (friendshipId) => {
-  return $.ajax({
+export const acceptFriend = (friendshipId) => (
+  $.ajax({
     method: 'PATCH',
     url: `/api/friendships/${friendshipId}`
-  });
-};
+  })
+);
 
-export const findFriendRequests = () => {
-  return $.ajax({
+export const findFriendRequests = () => (
+  $.ajax({
     method: 'GET',
     url: '/api/friendships'
-  });
-};
+  })
+);
