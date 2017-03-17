860c76c8a0266d9a541fbdba9833ac14ff24c2d6
Adom Hartell <adomhartell@gmail.com>
2017-03-16 20:47:38 -0700

    add post reducer

---
+++ frontend/reducers/post_reducer.js
@@ -0,0 +1,22 @@
+import { CREATE_POST, RECEIVE_POSTS, RECEIVE_POST, DELETE_POST }
+  from '../actions/post_actions';
+import merge from 'lodash/merge';
+
+const PostReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch(action.type){
+    case RECEIVE_POSTS:
+      return action.posts;
+    case RECEIVE_POST:
+      return Object.assign({}, state, {[action.post.id]: action.post});
+    case DELETE_POST:
+      const newPosts = Object.assign({}, state);
+      delete newPosts[action.postId];
+      return newPosts;
+    default:
+      return state;
+  }
+};
+
+export default PostReducer;
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import UserReducer from './user_reducer';
 import FriendshipReducer from './friendship_reducer';
+import PostReducer from './post_reducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
   user: UserReducer,
-  friendship: FriendshipReducer
+  friendship: FriendshipReducer,
+  post: PostReducer
 });
 
 export default rootReducer;
