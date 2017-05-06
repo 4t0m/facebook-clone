7e3c3fbccaf72eb862e3e5d00b96fa320c58cb69
Adom Hartell <adomhartell@gmail.com>
2017-05-05 23:02:46 -0700

    add birthdates to author seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,49 +14,59 @@ guest.save!
 dfw = User.create!(fname: "David", lname: "Foster Wallace", email: "dfw@domain.com", password: "password")
 dfw.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/002/original/DFW.jpg"
 dfw.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+dfw.birthday = "1962-02-21"
 dfw.save!
 
 wilde = User.create!(fname: "Oscar", lname: "Wilde", email: "wilde@domain.com", password: "password")
 wilde.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/003/original/Wilde.jpg"
 wilde.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+wilde.birthday = "1854-10-16"
 wilde.save!
 
 adams = User.create!(fname: "Douglas", lname: "Adams", email: "adams@domain.com", password: "password")
 adams.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/004/original/Adams.jpg"
 adams.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+adams.birthday = "1952-03-11"
 adams.save!
 
 sanderson = User.create!(fname: "Brandon", lname: "Sanderson", email: "sanderson@domain.com", password: "password")
 sanderson.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/005/original/Sanderson.jpg"
 sanderson.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+sanderson.birthday = "1975-12-19"
 sanderson.save!
 
 salinger = User.create!(fname: "J. D.", lname: "Salinger", email: "salinger@domain.com", password: "password")
 salinger.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/006/original/Salinger.jpg"
 salinger.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+salinger.birthday = "1919-01-01"
 salinger.save!
 
 plath = User.create!(fname: "Sylvia", lname: "Plath", email: "plath@domain.com", password: "password")
 plath.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/007/original/Plath.jpg"
 plath.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+plath.birthday = "1932-10-27"
 plath.save!
 
 vinge = User.create!(fname: "Vernor", lname: "Vinge", email: "vinge@domain.com", password: "password")
 vinge.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/008/original/Vinge.jpg"
 vinge.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+vinge.birthday = "1944-10-02"
 vinge.save!
 
 vonnegut = User.create!(fname: "Kurt", lname: "Vonnegut", email: "vonnegut@domain.com", password: "password")
 vonnegut.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/009/original/Vonnegut.jpg"
 vonnegut.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+vonnegut.birthday = "1922-11-11"
 vonnegut.save!
 
 stephenson = User.create!(fname: "Neal", lname: "Stephenson", email: "stephenson@domain.com", password: "password")
 stephenson.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/010/original/Stephenson.jpg"
 stephenson.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+stephenson.birthday = "1959-10-31"
 stephenson.save!
 
 grossman = User.create!(fname: "Lev", lname: "Grossman", email: "grossman@domain.com", password: "password")
-grossman.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/010/original/Grossman.jpg"
+grossman.profile_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/profile_pics/000/000/011/original/Grossman.jpg"
 grossman.cover_pic_url = "https://s3-us-west-1.amazonaws.com/facebookclone-pro/users/cover_pics/no-cover.png"
+grossman.birthday = "1969-06-26"
 grossman.save!
