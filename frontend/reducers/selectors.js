6f92d5e3def98eac35e2357d835bbd0212f7b278
Adom Hartell <adomhartell@gmail.com>
2017-05-09 21:53:17 -0700

    sort posts by date instead of id in selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,6 +1,5 @@
 export const getPosts = (posts) => {
-	let allPosts = Object.keys(posts).map(id => posts[id]);
-	return allPosts.sort( (postA, postB) => postB.id - postA.id);
+	return Object.keys(posts).length === 0 ? [] : posts;
 };
 
 export const getPost = (posts, postId) => (
