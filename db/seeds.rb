4a903433e710c511a80354456552af03bbdc0df1
Adom Hartell <adomhartell@gmail.com>
2017-05-11 23:30:21 -0700

    clean up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -296,11 +296,11 @@ post_strings = [
   Friendship.create!(user1_id: 1, user2_id: author_id, status: "accepted")
 
   post_strings[author_id - 2].each do |author_quote|
-      post = Post.create!(
-        body: author_quote, author_id: author_id, host_id: author_id, thumbnail_file_name: nil, thumbnail_content_type: nil, thumbnail_file_size: nil, thumbnail_updated_at: nil
-      )
-      post.thumbnail_from_url(author_pics[author_id - 2])
-      post.created_at = (rand * 10).days.ago
-      post.save!
+    post = Post.create!(
+      body: author_quote, author_id: author_id, host_id: author_id, thumbnail_file_name: nil, thumbnail_content_type: nil, thumbnail_file_size: nil, thumbnail_updated_at: nil
+    )
+    post.thumbnail_from_url(author_pics[author_id - 2])
+    post.created_at = (rand * 10).days.ago
+    post.save!
   end
 end
