005ebe08c6043f0ec53d5197c3618fa7bccd6fc5
Adom Hartell <adomhartell@gmail.com>
2017-05-14 15:29:40 -0700

    clean up entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
   //TODO Remove some of this (window asssignments)
   window.store = store;
 
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
